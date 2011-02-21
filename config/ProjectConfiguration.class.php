2ca022bdee20ee0a9cdcdcc27f84684d754ea18f
Adrien Couet <adrien.couet@keyconsulting.fr>
2011-02-21 12:14:17 +0100

    Commit des fichiers initiaux

---
+++ config/ProjectConfiguration.class.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once 'C://frameworks//symfony//symfony-1.4.9//lib/autoload/sfCoreAutoload.class.php';
+sfCoreAutoload::register();
+
+class ProjectConfiguration extends sfProjectConfiguration
+{
+  public function setup()
+  {
+    $this->enablePlugins('sfDoctrinePlugin');
+  }
+}
