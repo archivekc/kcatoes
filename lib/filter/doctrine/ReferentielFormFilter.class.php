c4f685d9519b94d4c617356008849dce4c93ddf8
Adrien Couet <adrien.couet@keyconsulting.fr>
2011-02-23 16:06:45 +0100

    Création du schéma et commit des fichiers générés

---
+++ lib/filter/doctrine/ReferentielFormFilter.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Referentiel filter form.
+ *
+ * @package    kcatoes
+ * @subpackage filter
+ * @author     Your name here
+ * @version    SVN: $Id: sfDoctrineFormFilterTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class ReferentielFormFilter extends BaseReferentielFormFilter
+{
+  public function configure()
+  {
+  }
+}
---
+++ lib/filter/doctrine/RegroupementFormFilter.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Regroupement filter form.
+ *
+ * @package    kcatoes
+ * @subpackage filter
+ * @author     Your name here
+ * @version    SVN: $Id: sfDoctrineFormFilterTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class RegroupementFormFilter extends BaseRegroupementFormFilter
+{
+  public function configure()
+  {
+  }
+}
