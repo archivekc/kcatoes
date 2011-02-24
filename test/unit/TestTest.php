fbb9064c18caabe7d40f3a53499cf8d2767689a7
Adrien Couet <adrien.couet@keyconsulting.fr>
2011-02-24 16:12:05 +0100

    Implémentation du test de Test->getNomCourt()

--- test/unit/TestTest.php
+++ test/unit/TestTest.php
@@ -1,26 +1,26 @@
 <?php
-require_once dirname(__FILE__).'/../bootstrap/unit.php';
+require_once dirname(__FILE__).'/../bootstrap/Doctrine.php';
 
 $t = new lime_test(7);
-//$test = new Test();
-//
-//$test->setNom('Toto');
-//$t->is($test->getNomCourt(), 'Toto', '\'Toto\' => \'Toto\'');
-//
-//$test->setNom('toto-toto');
-//$t->is($test->getNomCourt(), 'TotoToto', '\'toto-toto\' => \'TotoToto\'');
-//
-//$test->setNom('Toto toto');
-//$t->is($test->getNomCourt(), 'TotoToto', '\'Toto toto\' => \'TotoToto\'');
-//
-//$test->setNom('TotoToTo');
-//$t->is($test->getNomCourt(), 'Totototo', '\'TotoToTo\' => \'Totototo\'');
-//
-//$test->setNom('toto');
-//$t->is($test->getNomCourt(), 'Toto', '\'toto\' => \'Toto\'');
-//
-//$test->setNom('tété');
-//$t->is($test->getNomCourt(), 'Tete', '\'tété\' => \'Tete\'');
-//
-//$test->setNom('tété@toto.fr');
-//$t->is($test->getNomCourt(), 'TeteTotoFr', '\'tété@toto.fr\' => \'TeteTotoFr\'');
+$test = new Test();
+
+$test->setNom('Toto');
+$t->is($test->getNomCourt(), 'Toto', 'Test nominal: \'Toto\' => \'Toto\'');
+
+$test->setNom('toto-toto');
+$t->is($test->getNomCourt(), 'TotoToto', '\'toto-toto\' => \'TotoToto\'');
+
+$test->setNom('Toto toto');
+$t->is($test->getNomCourt(), 'TotoToto', '\'Toto toto\' => \'TotoToto\'');
+
+$test->setNom('TotoToTo');
+$t->is($test->getNomCourt(), 'Totototo', '\'TotoToTo\' => \'Totototo\'');
+
+$test->setNom('toto');
+$t->is($test->getNomCourt(), 'Toto', '\'toto\' => \'Toto\'');
+
+$test->setNom('tété');
+$t->is($test->getNomCourt(), 'Tete', '\'tété\' => \'Tete\'');
+
+$test->setNom('tété@toto.fr');
+$t->is($test->getNomCourt(), 'TeteTotoFr', '\'tété@toto.fr\' => \'TeteTotoFr\'');
