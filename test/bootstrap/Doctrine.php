6527e99e74fbbb71572bf764bab1d08b7b5710ce
Adrien Couet <adrien.couet@keyconsulting.fr>
2011-02-24 16:14:34 +0100

    Configuration de l'environnement de test

---
+++ test/bootstrap/Doctrine.php
@@ -0,0 +1,14 @@
+<?php
+
+include(dirname(__FILE__).'/unit.php');
+
+$configuration = ProjectConfiguration::getApplicationConfiguration('frontend', 'test', true);
+new sfDatabaseManager($configuration);
+//Doctrine::dropDatabases();
+//Doctrine::createDatabases();
+//Doctrine::loadModels(sfConfig::get('sf_lib_dir') . '/model/doctrine', Doctrine::MODEL_LOADING_CONSERVATIVE);
+//Doctrine::createTablesFromArray(Doctrine::getLoadedModels());
+//Doctrine::loadData(sfConfig::get('sf_data_dir').'/fixtures/test', false);
+
+// remove all cache
+//sfToolkit::clearDirectory(sfConfig::get('sf_app_cache_dir'));
