a34fca6569d612bc2860be91836c9726139a6bf8
Adrien Couet <adrien.couet@keyconsulting.fr>
2011-02-25 10:55:31 +0100

    Ajout de nouveau cas de test pour la fonction Test->getNomCourt()

--- test/unit/TestTest.php
+++ test/unit/TestTest.php
@@ -1,11 +1,14 @@
 <?php
 require_once dirname(__FILE__).'/../bootstrap/Doctrine.php';
 
-$t = new lime_test(7);
+$t = new lime_test(10);
 $test = new Test();
 
+$t->comment('Generation du nom court');
+$t->comment('\'Nom du test\' => \'Nom court attendu\'');
+
 $test->setNom('Toto');
-$t->is($test->getNomCourt(), 'Toto', 'Test nominal: \'Toto\' => \'Toto\'');
+$t->is($test->getNomCourt(), 'Toto', '\'Toto\' => \'Toto\'');
 
 $test->setNom('toto-toto');
 $t->is($test->getNomCourt(), 'TotoToto', '\'toto-toto\' => \'TotoToto\'');
@@ -19,8 +22,17 @@ $t->is($test->getNomCourt(), 'Totototo', '\'TotoToTo\' => \'Totototo\'');
 $test->setNom('toto');
 $t->is($test->getNomCourt(), 'Toto', '\'toto\' => \'Toto\'');
 
-$test->setNom('tété');
-$t->is($test->getNomCourt(), 'Tete', '\'tété\' => \'Tete\'');
+$test->setNom('éèàçùûêî');
+$t->is($test->getNomCourt(), 'Eeacuuei', '\'éèàçùûêî\' => \'Eeacuuei\'');
 
 $test->setNom('tété@toto.fr');
 $t->is($test->getNomCourt(), 'TeteTotoFr', '\'tété@toto.fr\' => \'TeteTotoFr\'');
+
+$test->setNom('  tro  ');
+$t->is($test->getNomCourt(), 'Tro', '\'  tro  \' => \'Tro\'');
+
+$test->setNom("\t toto \n");
+$t->is($test->getNomCourt(), 'Toto', '\'\t toto \n\' => \'Toto\'');
+
+$test->setNom('test numero 10');
+$t->is($test->getNomCourt(), 'TestNumero10', '\'test numero 10\' => \'TestNumero10\'');
