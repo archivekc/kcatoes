bb543ffd9af95f3571339e823a4f341a06607ebe
Adrien Couet <adrien.couet@keyconsulting.fr>
2011-02-28 18:31:43 +0100

    Implémentation des derniers tests de validité d'URL Passage à une gestion des erreurs de validité par Exception

--- lib/model/Page.class.php
+++ lib/model/Page.class.php
@@ -11,62 +11,70 @@ use Goutte\Client;
  */
 class Page extends Client
 {
-  private $url;
+	private $url;
+	private $logger;
 
-  public function __construct($_url)
-  {
-  	$this->url = $_url;
-  	parent::__construct();
-  }
+	public function __construct($_url, $_logger = null)
+	{
+		$this->url = $_url;
+		$this->logger = $_logger;
+		parent::__construct();
+	}
 
-  public function __get($var)
-  {
-  	return $this->$var;
-  }
+	public function __get($var)
+	{
+		return $this->$var;
+	}
 
-  /**
-   * Gère les get et les set pour les attributs de la classe
-   *
-   * @throws Exception
-   */
-//  public function __call($method, $arguments)
-//  {
-//  	if (in_array($verb = substr($method, 0, 3), array('set', 'get')))
-//  	{
-//  		$name = strtolower(substr($method, 3));
-//
-//  		if (in_array($name, get_class_vars(get_class($this))))
-//  		{
-//  			if ($verb == 'get')
-//  			{
-//  				return $this->$name;
-//  			}
-//  			else
-//  			{
-//  				$this->$name = $arguments[0];
-//  			}
-//  		}
-//  		else
-//  		{
-//  			throw new Exception('Variable '.$name.' introuvable');
-//  		}
-//  	}
-//  }
+	/**
+	 * Gère les get et les set pour les attributs de la classe
+	 *
+	 * @throws Exception
+	 */
+	//  public function __call($method, $arguments)
+	//  {
+	//  	if (in_array($verb = substr($method, 0, 3), array('set', 'get')))
+	//  	{
+	//  		$name = strtolower(substr($method, 3));
+	//
+	//  		if (in_array($name, get_class_vars(get_class($this))))
+	//  		{
+	//  			if ($verb == 'get')
+	//  			{
+	//  				return $this->$name;
+	//  			}
+	//  			else
+	//  			{
+	//  				$this->$name = $arguments[0];
+	//  			}
+	//  		}
+	//  		else
+	//  		{
+	//  			throw new Exception('Variable '.$name.' introuvable');
+	//  		}
+	//  	}
+	//  }
 
-  /**
-   * Vérifie la validité de l'url de la page puis génère son DOM
-   *
-   * @date 25/02/2011
-   */
-  public function buildCrawler()
-  {
-  	if(UrlValidation::isValide($this->url))
-  	{
-  		$this->request('GET', $this->url);
-  	}
-  	else
-  	{
-  		die();
-  	}
-  }
+	/**
+	 * Vérifie la validité de l'url de la page puis génère son crawler
+	 *
+	 * @date 25/02/2011
+	 */
+	public function buildCrawler()
+	{
+		try
+		{
+			UrlValidation::isValide($this->url);
+		}
+		catch(KcatoesUrlException $e)
+		{
+			$errorMessage = $e->getMessage();
+			if($this->logger instanceof sfLogger)
+			{
+        $this->logger->err($errorMessage);
+			}
+		  die($errorMessage);
+		}
+		$this->request('GET', $this->url);
+	}
 }
--- lib/model/UrlValidation.php
+++ lib/model/UrlValidation.php
@@ -5,53 +5,102 @@
  * @author Adrien Couet
  */
 
+use Zend\Validator\File\Count;
+use Zend\Db\Select;
 class UrlValidation
 {
-  /**
-   * Vérifie la validité d'une URL à partir des tests présents dans la classe
-   *
-   * @param $url URL à tester
-   */
+
+	/**
+	 * Vérifie la validité d'une URL à partir des tests présents dans la classe
+	 *
+	 * @param $url URL à tester
+	 * @tested
+	 */
 	public static function isValide($url)
 	{
-		$valide = true;
-		$valide = $valide && self::isSyntaxeValide($url);
-		$valide = $valide && self::isDnsValide($url);
+		self::isSyntaxeValide($url);
+		$meta = self::isDnsValide($url);
+		self::isCodeHttpValide($meta);
+		self::isFormatValide($meta);
 
-		return $valide;
+		return true;
 	}
 
 	/**
 	 * Vérifie si la syntaxe d'une URL est correcte
 	 *
 	 * @param $url URL à tester
+   * @tested
 	 */
-  public static function isSyntaxeValide($url)
-  {
-  	if(preg_match('#^http://([a-zA-Z0-9-]+.)?([a-zA-Z0-9-]+.)?[a-zA-Z0-9-]+\.[a-zA-Z]{2,4}(:[0-9]+)?(/[a-zA-Z0-9-]*)?(.[a-zA-Z0-9]{1,4})?#', $url))
-  	{
-  		return true;
-  	}
-  	else
-  	{
-      //sfContext::getInstance()->getLogger()->err('L\'URL indiquee ne respecte pas la convention d\'ecriture.');
-  		return false;
-  	}
-  }
+	public static function isSyntaxeValide($url)
+	{
+		if(!preg_match('#^http://([a-zA-Z0-9-]+.)?([a-zA-Z0-9-]+.)?[a-zA-Z0-9-]+\.[a-zA-Z]{2,4}(:[0-9]+)?(/[a-zA-Z0-9-]*)?(.[a-zA-Z0-9]{1,4})?#', $url))
+		{
+			throw new KcatoesUrlException('L\'URL indiquee ne respecte pas la convention d\'ecriture.');
+		}
+	}
 
-  public static function isDnsValide($url)
-  {
-  	//echo parse_url($url, PHP_URL_HOST);
-  	$socket = @fsockopen(parse_url($url, PHP_URL_HOST), 80, $errno, $errstr, 30);
-    if(!$socket)
-    {
-    	//sfContext::getInstance()->getLogger()->err('Echec lors de la connexion au serveur.');
-    	return false;
-    }
-    else
-    {
-    	fclose($socket);
-    	return true;
-    }
-  }
+	/**
+	 * Vérifie si le serveur pointé par une URL est accessible depuis
+	 * l'application en essayant de s'y connecter
+	 *
+	 * @param $url URL à tester
+   * @tested
+	 */
+	public static function isDnsValide($url)
+	{
+		$fp = @fopen($url, 'r');
+		if(!$fp)
+		{
+			throw new KcatoesUrlException('Echec lors de la connexion au serveur.');
+		}
+		else
+		{
+			$meta = stream_get_meta_data($fp);
+//			print_r($meta['wrapper_data']);
+			fclose($fp);
+			return $meta;
+		}
+	}
+
+
+	/**
+	 * Vérifie que le serveur renvoit bien un code HTTP 200 lors de l'accès
+	 * à la page désignée par une URL
+	 *
+	 * @param $meta Meta données de l'URL à tester
+   * @tested
+	 */
+	public static function isCodeHttpValide($meta)
+	{
+
+		$return = explode(' ', $meta['wrapper_data'][0]);
+
+		if(!$return[1] == 200)
+		{
+			throw new KcatoesUrlException('Le serveur a renvoye un code HTTP non valide: '.$return[1]);
+		}
+	}
+
+	/**
+	 * Vérifie que la page pointée par une URL est bien au format
+	 *
+	 * @param $meta Meta données de l'URL à tester
+   * @tested
+	 */
+	public static function isFormatValide($meta)
+	{
+		$found = false;
+		$i = 0;
+		while(!$found && $i < count($meta['wrapper_data']))
+		{
+			$return = explode(' ', $meta['wrapper_data'][$i]);
+			$found = $return[0] == 'Content-Type:';
+			$i++;
+		}
+    if(!preg_match('#text/html#', $return[1]) && !preg_match('#text/xml#', $return[1]))
+		{
+			throw new KcatoesUrlException('La page recuperee n\'est pas au format XHTML.');
+		}
+	}
 }
