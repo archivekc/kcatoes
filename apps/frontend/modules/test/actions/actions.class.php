cf4be3982a71d6510ccc6e38e2a8b772baee1a87
Adrien Couet <adrien.couet@keyconsulting.fr>
2011-03-01 14:16:18 +0100

    Code de test avec sortie écran

--- apps/frontend/modules/test/actions/actions.class.php
+++ apps/frontend/modules/test/actions/actions.class.php
@@ -5,8 +5,7 @@
  *
  * @package    kcatoes
  * @subpackage test
- * @author     Your name here
- * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ * @author     Adrien Couet
  */
 class testActions extends sfActions
 {
@@ -17,5 +16,48 @@ class testActions extends sfActions
   */
   public function executeIndex(sfWebRequest $request)
   {
+//    $test = new Test();
+//    $test->setNom("\t toto \n");
+//    $this->nom = $test->getNom();
+//    $this->nomCourt = $test->getNomCourt();
+//    $this->urlDeTest = 'http://www.keyconsulting.fr/'; //Valide
+//    $this->urlDeTest = 'www.jesuisdeconnecte.fr';      //404
+    $this->urlDeTest = 'http://abonnes.lemonde.fr/';   //302
+//    $this->urlDeTest = 'toto';                         //Syntaxe invalide
+//    $this->urlDeTest = 'http://www.keyconsulting.fr/images/KeyConsulting.jpg'; //Format invalide
+
+    $page = new page($this->urlDeTest, sfContext::getInstance()->getLogger());
+
+    try
+    {
+    	$page->buildCrawler();
+    } catch (KcatoesCrawlerException $e)
+    {
+    	$this->getRequest()->setParameter('errorMessage', $e->getMessage());
+    	$this->forward('Test', 'Erreur');
+    }
+    $crawler = $page->crawler;
+    $nodes = $crawler->filter('div');
+    $this->nbDiv = $nodes->count();
+  }
+
+  public function executeErreur(sfWebRequest $request)
+  {
+  	$this->errorMessage = $this->getRequest()->getParameter('errorMessage');
+  }
+
+ /**
+  * Executes index action
+  *
+  * @param sfRequest $request A request object
+  */
+  public function executeGoutte(sfWebRequest $request)
+  {
+    $this->urlDeTest = 'http://www.keyconsulting.fr/';
+
+    $page = new Page();
+    $crawler = $page->request('GET', $this->urlDeTest);
+    $nodes = $crawler->filter('div');
+    $this->nbDiv = $nodes->count();
   }
 }
---
+++ apps/frontend/modules/test/templates/indexSuccess.php
@@ -0,0 +1,3 @@
+<p>Test d'url sur <?php echo $urlDeTest ?></p>
+
+<p>Nombre de div : <?php echo $nbDiv ?></p>
