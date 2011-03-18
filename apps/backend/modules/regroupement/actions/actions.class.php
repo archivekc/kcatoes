4a46e9eddd921e2de06866e011cb14a95e86f8af
Adrien Couet <adrien.couet@keyconsulting.fr>
2011-03-18 10:07:40 +0100

    Ajout des fichiers du CRUD généré par symfony pour les différents objets métier de l'application

---
+++ apps/backend/modules/regroupement/actions/actions.class.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once dirname(__FILE__).'/../lib/regroupementGeneratorConfiguration.class.php';
+require_once dirname(__FILE__).'/../lib/regroupementGeneratorHelper.class.php';
+
+/**
+ * regroupement actions.
+ *
+ * @package    kcatoes
+ * @subpackage regroupement
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class regroupementActions extends autoRegroupementActions
+{
+}
---
+++ apps/backend/modules/regroupement/lib/regroupementGeneratorConfiguration.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * regroupement module configuration.
+ *
+ * @package    kcatoes
+ * @subpackage regroupement
+ * @author     Your name here
+ * @version    SVN: $Id: configuration.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class regroupementGeneratorConfiguration extends BaseRegroupementGeneratorConfiguration
+{
+}
