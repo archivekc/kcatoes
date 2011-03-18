c3e3f4595144bbd7644de2c257bf6cb91c518afd
Adrien Couet <adrien.couet@keyconsulting.fr>
2011-03-18 18:14:40 +0100

    Ajout d'un menu pour naviguer dans le backend

--- apps/backend/templates/layout.php
+++ apps/backend/templates/layout.php
@@ -1,14 +1,34 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
   <head>
-    <?php include_http_metas() ?>
-    <?php include_metas() ?>
-    <?php include_title() ?>
+    <title>KCatoes Admin Interface</title>
     <link rel="shortcut icon" href="/favicon.ico" />
-    <?php include_stylesheets() ?>
+    <?php use_stylesheet('admin.css') ?>
     <?php include_javascripts() ?>
+    <?php include_stylesheets() ?>
   </head>
   <body>
-    <?php echo $sf_content ?>
+    <div id="container">
+      <div id="menu">
+        <ul>
+          <li>
+            <?php echo link_to('Thématiques', 'thematique') ?>
+          </li>
+          <li>
+            <?php echo link_to('Référentiels', 'referentiel') ?>
+          </li>
+          <li>
+            <?php echo link_to('Regroupements', 'regroupement') ?>
+          </li>
+          <li>
+            <?php echo link_to('Tests', 'test') ?>
+          </li>
+        </ul>
+      </div>
+      <div id="content">
+        <?php echo $sf_content ?>
+      </div>
+    </div>
   </body>
 </html>
