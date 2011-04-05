c2f063d1a05407e315e3957bbc5816a09ec05372
Antoine Rolland <antoine.rolland@keyconsulting.fr>
2011-04-05 18:56:12 +0200

    Correction indentation

--- apps/frontend/modules/test/templates/executeSuccess.php
+++ apps/frontend/modules/test/templates/executeSuccess.php
@@ -4,6 +4,7 @@
 
 <p class="erreur"><?php echo $erreur ?></p>
 <p class="info"><?php echo $info ?></p>
+
 <?php if($cheminFichierCsv != ''): ?>
-<p><a href="<?php echo transformForWeb($cheminFichierCsv) ?>">Télécharger le fichier CSV</a></p>
+  <p><a href="<?php echo transformForWeb($cheminFichierCsv) ?>">Télécharger le fichier CSV</a></p>
 <?php endif ?>
