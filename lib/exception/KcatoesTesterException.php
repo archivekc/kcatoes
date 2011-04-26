f122ecde87ca8498ef8cd2d1c997b8da6d87110a
Adrien Couet <adrien.couet@keyconsulting.fr>
2011-04-26 18:13:20 +0200

    Ajout d'une exception pour gérer les erreurs propres à l'objet Tester

---
+++ lib/exception/KcatoesTesterException.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Gère les erreurs liées à l'utilisation du tester
+ *
+ * @author Adrien Couet <adrien.couet@keyconsulting.fr>
+ */
+class KcatoesTesterException extends KcatoesMetException
+{
+
+}
