cc31148773c93a2a3641cf5c21c3169d26582879
Adrien Couet <adrien.couet@keyconsulting.fr>
2011-04-28 17:19:29 +0200

    Correction du proof of concept de module de test unitaire d'implémentation

--- test/unit/ImplementationTest.php
+++ test/unit/ImplementationTest.php
@@ -4,12 +4,11 @@ require_once dirname(__FILE__).'/../bootstrap/Doctrine.php';
 
 $t = new lime_test(1);
 
-$content = file_get_contents('http://dev.kcatoes.local/dev/test.html');
-$page = new Page($content, 'http://dev.kcatoes.local/dev/test.html');
+$page = new Page('', 'http://dev.kcatoes.local/dev/test.html');
 $page->buildCrawler();
 
 $test = new AbsenceDeLElementBlink();
 $result = $test->execute($page);
 
 $t->comment('Proof of concept du test unitaire de l\'implémentation d\'un test');
-$t->is($result, Resultat::ECHEC);
+$t->is($result, Resultat::REUSSITE);
