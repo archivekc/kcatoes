a2800e43b5f6d87dea94c7ab2203bdb7f7f36811
Adrien Couet <adrien.couet@keyconsulting.fr>
2011-04-28 17:14:11 +0200

    Correction du proof of concept de module de test d'implementation

--- test/unit/ImplementationTest.php
+++ test/unit/ImplementationTest.php
@@ -4,11 +4,12 @@ require_once dirname(__FILE__).'/../bootstrap/Doctrine.php';
 
 $t = new lime_test(1);
 
-$page = new Page('http://dev.kcatoes.local/dev/test.html');
+$content = file_get_contents('http://dev.kcatoes.local/dev/test.html');
+$page = new Page($content, 'http://dev.kcatoes.local/dev/test.html');
 $page->buildCrawler();
 
 $test = new AbsenceDeLElementBlink();
-$test->execute($page);
+$result = $test->execute($page);
 
 $t->comment('Proof of concept du test unitaire de l\'implémentation d\'un test');
-$t->is($test->getExplication(), 'La page contient 1 élément(s) blink');
+$t->is($result, Resultat::ECHEC);
