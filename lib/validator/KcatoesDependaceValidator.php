14000d2d0dc8fa6ed2b7de8608504ce1a40cfbdc
Adrien Couet <adrien.couet@keyconsulting.fr>
2011-04-28 11:18:40 +0200

    Correction du validateur de dépendance et ajout de fonctionnalités:  - si une dépendance est définie, sa condition de dépendance l'est aussi  - pour clarifier la BDD, si aucune dépendance n'est définie alors la condition de dépendance est mise à null

--- lib/validator/KcatoesDependaceValidator.php
+++ lib/validator/KcatoesDependaceValidator.php
@@ -1,5 +1,17 @@
 <?php
 
+/**
+ * Validateur appelé lors de l'ajout ou la modification d'un test via le
+ * formulaire généré par Symfony.
+ * Vérifie qu'aucune dépendance cyclique n'est créée (un test faisant partie de
+ * ses dépendances) et que si le test est défini comme dépendant, alors le
+ * champ 'execute_si' est également renseigné.
+ * Pour clarifier la base de données, si aucune dépendance n'est définie alors
+ * le champ 'execute_si' n'est pas renseigné.
+ *
+ * @author Adrien Couet <adrien.couet@keyconsulting.fr>
+ *
+ */
 class KcatoesDependanceValidator extends sfValidatorDoctrineUnique
 {
   /**
@@ -9,25 +21,39 @@ class KcatoesDependanceValidator extends sfValidatorDoctrineUnique
   {
     $clean = parent::doClean($values);
 
-    $currentId = $values['id'];
-    $errorMessage = 'Un test ne peut pas faire partie de ses propres dépendances';
-    $dependances = array();
-
-    $parent = TestTable::getInstance()->findOneById($clean['dependance_id']);
-    array_merge($dependances, $parent->getExecutionList());
-    $dependances[] = $parent;
-
-    sfContext::getInstance()->getLogger()->info($parent);
-
-    foreach ($dependances as $dependance)
+    if ($clean['dependance_id'] != null)
     {
-      sfContext::getInstance()->getLogger()->info($dependance);
+      $currentId = $clean['id'];
+      $parent = TestTable::getInstance()->findOneById($clean['dependance_id']);
+      $dependances = $parent->getExecutionList();
+      $dependances[] = $parent;
 
-      if ($dependance->getId() == $currentId)
+      foreach ($dependances as $dependance)
       {
-        $error = new sfValidatorError($this, $errorMessage, array('column' => 'id', 'column' => 'dependance_id'));
-        throw new sfValidatorErrorSchema($this, array('dependance_id' => $error));
+        if ($dependance->getId() == $currentId)
+        {
+          $error = new sfValidatorError(
+            $this,
+            'Un test ne peut pas faire partie de ses propres dépendances',
+            array('column' => 'id', 'column' => 'dependance_id')
+          );
+          throw new sfValidatorErrorSchema($this, array('dependance_id' => $error));
+        }
       }
+
+      if ($clean['execute_si'] == null)
+      {
+        $error = new sfValidatorError(
+          $this,
+          'Vous devez préciser la condition de dépendance',
+          array('column' => 'execute_si')
+        );
+        throw new sfValidatorErrorSchema($this, array('execute_si' => $error));
+      }
+    }
+    else
+    {
+      $clean['execute_si'] = null;
     }
 
     return $clean;
