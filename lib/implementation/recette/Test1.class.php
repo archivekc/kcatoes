0b1cb9f8c1da73a7dd3047bca8883e311c560ff8
Adrien Couet <adrien.couet@keyconsulting.fr>
2011-04-29 15:53:51 +0200

    Ajout des données de recette

---
+++ lib/implementation/recette/Test1.class.php
@@ -0,0 +1,12 @@
+﻿<?php	
+class Test3 extends ASource
+{
+  public function __construct()
+  {
+  }
+
+  public function execute(Page $page)
+  {
+    return Resultat::ECHEC;
+  }
+}
---
+++ lib/implementation/recette/Test9.class.php
@@ -0,0 +1,12 @@
+﻿<?php	
+class Test9 extends ASource
+{
+  public function __construct()
+  {
+  }
+
+  public function execute(Page $page)
+  {
+    throw new KcatoesTestException('Erreur d\'exécution volontaire');
+  }
+}
