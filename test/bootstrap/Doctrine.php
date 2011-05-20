9353b3cc375953f4a11bbd669b4218f6f57f8b10
Adrien Couet <adrien.couet@keyconsulting.fr>
2011-05-20 16:34:38 +0200

    Tentative de fix du build Jenkins

--- test/bootstrap/Doctrine.php
+++ test/bootstrap/Doctrine.php
@@ -3,12 +3,12 @@
 include(dirname(__FILE__).'/unit.php');
 
 $configuration = ProjectConfiguration::getApplicationConfiguration('frontend', 'test', true);
-new sfDatabaseManager($configuration);
-Doctrine::dropDatabases();
-Doctrine::createDatabases();
-Doctrine::loadModels(sfConfig::get('sf_lib_dir') . '/model/doctrine', Doctrine::MODEL_LOADING_CONSERVATIVE);
-Doctrine::createTablesFromArray(Doctrine::getLoadedModels());
-Doctrine::loadData(sfConfig::get('sf_test_dir').'/fixtures');
+//new sfDatabaseManager($configuration);
+//Doctrine::dropDatabases();
+//Doctrine::createDatabases();
+//Doctrine::loadModels(sfConfig::get('sf_lib_dir') . '/model/doctrine', Doctrine::MODEL_LOADING_CONSERVATIVE);
+//Doctrine::createTablesFromArray(Doctrine::getLoadedModels());
+//Doctrine::loadData(sfConfig::get('sf_test_dir').'/fixtures');
 
 // remove all cache
 sfToolkit::clearDirectory(sfConfig::get('sf_app_cache_dir'));
