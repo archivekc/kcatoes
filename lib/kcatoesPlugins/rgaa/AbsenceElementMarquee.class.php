8e14ea1e842a72c74c8170d43f337a08ada95dc3
JTA <julien.tardot@keyconsulting.fr>
2012-03-08 16:57:40 +0100

    Test - 5.23 - AbsenceElementMarquee

---
+++ lib/kcatoesPlugins/rgaa/AbsenceElementMarquee.class.php
@@ -0,0 +1,39 @@
+<?php
+namespace Kcatoes\rgaa;
+
+
+
+class AbsenceElementMarquee extends \ASource
+{
+  
+  const testName = 'Absence d\'élément marquee';
+  const testId = '5.23';
+  protected $testProc = array(
+    'Si l\'élément marquee est absent, le test est validé, sinon le test est invalidé.' 
+  );
+  protected $testDocLinks = array(
+    'G186' => 'http://www.w3.org/TR/WCAG20-TECHS/G186.html',
+    'F16'  => 'http://www.w3.org/TR/2010/NOTE-WCAG20-TECHS-20101014/F16.html'
+  );
+  
+  
+  public function execute()
+  {
+    $crawler = $this->page->crawler;
+    
+    $elements   = 'marquee';
+
+    $nodes = $crawler->filter($elements);
+
+    if (count($nodes) == 0) {
+      $this->addResult(null, \Resultat::REUSSITE, 'Il n\'y a pas d\'élément marquee');
+    }
+    else {
+      foreach($nodes as $node) {
+        $this->addResult($node, \Resultat::ECHEC, 'Il y a un élément marquee');
+      }
+    }
+
+  }
+}
+  
