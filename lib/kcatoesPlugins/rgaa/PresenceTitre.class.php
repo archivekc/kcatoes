71f42b74c882c86174e1bea90b447f7c2a9c31bf
JTA <julien.tardot@keyconsulting.fr>
2012-03-09 16:13:27 +0100

    Test - 9.6 - PresenceTitre

---
+++ lib/kcatoesPlugins/rgaa/PresenceTitre.class.php
@@ -0,0 +1,42 @@
+<?php
+namespace Kcatoes\rgaa;
+
+
+
+class PresenceTitre extends \ASource
+{
+  
+  const testName = 'A - Présence d\'un titre dans la page';
+  const testId = '9.6';
+  protected $testProc = array(
+    'Si l\'élément mentionné dans le champ d\'application est présent dans la page, le test est validé, sinon le test est invalidé.' 
+  );
+  protected $testDocLinks = array(
+    'G88' => 'http://www.w3.org/TR/WCAG20-TECHS/G88.html',
+    'H25' => 'http://www.w3.org/TR/WCAG20-TECHS/H25.html'
+  );
+  
+  
+  public function execute()
+  {
+    $crawler = $this->page->crawler;
+
+    $elements   = 'title';
+
+    $nodes = $crawler->filter($elements);
+
+    if (count($nodes) > 0) {
+      $nodes->rewind();
+      $title = $nodes->current();
+      $val = trim($title->nodeValue);
+      
+      if (strlen($val) > 0) {
+        $this->addResult($title, \Resultat::REUSSITE, 'L\'élément title est présent : ' . $val);
+        return;
+      }
+    }
+
+    $this->addResult(null, \Resultat::ECHEC, 'Pas d\'élément title dans la page ou celui-ci est vide');
+    
+  }
+}
