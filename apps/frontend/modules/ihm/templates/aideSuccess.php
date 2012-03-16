6fc7d43be823580a9a34bca2d575ff271ef48e90
CFA <cyril.fabby@keyconsulting.fr>
2012-03-16 17:06:53 +0100

    Page d'aide

---
+++ apps/frontend/modules/ihm/templates/aideSuccess.php
@@ -0,0 +1,25 @@
+<h1>Aide de KCatoès</h1>
+
+<h2>Utilisation</h2>
+<p>La présente interface propose d'associer des pages web avec des configuration de tests.</p>
+<dl>
+  <dt>Page web&nbsp;:</dt>
+  <dd>Une page est représentée par son url</dd>
+  <dt>Configuration de test&nbsp;:</dt>
+  <dd>Une configuration de test représente un ensemble de test qu'il convient de passer sur une page web. Elle identifiée par un libellé.</dd>
+</dl>
+<p>Il est possible d'ajouter des pages web et des configurations de test depuis la page d'accueil.</p>
+<p>Pour chaque page web et pour chaque configuration de test, un écran est disponible (accessible par un lien dans la page d'accueil.</p>
+
+<h2>Configuration de test</h2>
+<p>L'écran d'une configuration de test permet de définir les tests (parmi tous les tests disponibles).</p>
+
+<h2>Page web</h2>
+<p>L'écran d'une page web permet d'associer une ou plusierus configuration de tests à la page web.</p>
+<p>Une fois l'association faite, les tests sont lancés.</p>
+<p>Une page de confirmation est affichée. Elle permet de voir la liste des tests qui ont été passés. Il est également possible de visualiser l'interface d'évaluation qui a été générée</p>
+<p>Si les tests avaient déjà été lancés, alors pour chaque association deux liens sont présentés&nbsp;:</p>
+<ul>
+  <li>Un lien vers la dernière version du rapport d'évaluation.</li>
+  <li>Un lien vers la dernière sauvegarde automatique (si elle existe) du rapport d'évaluation.</li>
+</ul>
