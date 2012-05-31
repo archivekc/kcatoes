f351906eb59a9fd0a2ad10cee691f1e9d78e6144
JTA <julien.tardot@keyconsulting.fr>
2012-05-31 17:21:09 +0200

    Infos de l'utilisateur connecté

--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -43,6 +43,7 @@
 		            <a href="<?php echo url_for('editorial')?>">Documentation</a>
 		          </li>
 	          </ul>
+            
 	          <?php if($sf_user->isAuthenticated() && $sf_user->hasCredential('admin')): ?>
 		        <strong>Administration</strong>
 		        <ul class="menu">
@@ -55,18 +56,29 @@
 		          <li>
 		            <a href="<?php echo url_for('sf_guard_permission')?>">Gestion des rôles</a>
 		          </li>
-  	        </ul>
-	          <?php endif ?>
+            </ul>
+            <?php endif ?>
+
             <p id="copyright">
               Version <?php echo sfConfig::get('app_version') ?> -
               <?php echo sfConfig::get('app_date') ?>
             </p>
 		      </div>
+          
 		      <?php if($sf_user->isAuthenticated()): ?>
           <div id="logoutBtn">
-              <a class="actionButton" href="<?php echo url_for('sf_guard_signout')?>">Déconnexion</a>
+            <div id="connectedUser">
+              <?php echo $sf_user->getName(); ?>
+              <ul id="userGroups">
+                <?php foreach($sf_user->getGroups() as $group): ?>
+                  <li><?php echo $group->getName(); ?></li> 
+                <?php endforeach; ?>
+              </ul>
+            </div>
+            <a class="actionButton" href="<?php echo url_for('sf_guard_signout')?>">Déconnexion</a>
           </div>
           <?php endif ?>
+          
       </div>
       <div id="main">
         <div id="page">
