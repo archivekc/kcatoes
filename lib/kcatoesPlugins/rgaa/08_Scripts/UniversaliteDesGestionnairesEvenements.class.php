4a62c4f36397ef712ac3ade19137fee416af4b66
Geoffroy GUERITOT <geoffroy.gueritot@keyconsulting.fr>
2012-06-04 11:33:27 +0200

    mise a jour selecteur test de script 8.3

--- lib/kcatoesPlugins/rgaa/08_Scripts/UniversaliteDesGestionnairesEvenements.class.php
+++ lib/kcatoesPlugins/rgaa/08_Scripts/UniversaliteDesGestionnairesEvenements.class.php
@@ -35,6 +35,19 @@ class UniversaliteDesGestionnairesEvenements extends \ASource
 
   public function execute()
   {
-    $this->addResult(null, \Resultat::NA, 'Test non implémenté');
+  $crawler = $this->page->crawler;
+
+    $elements   = 'applet, object, img, input[type=text], input[type=checkbox], input[type=file], input[type=radio], input[type=password], select, textarea';
+
+    $nodes = $crawler->filter($elements);
+
+    if (count($nodes) == 0) {
+      $this->addResult(null, \Resultat::NA, 'Il n\'y a pas d\'élément ayant des attributs de gestion d\'évènements');
+    }
+    else {
+      foreach($nodes as $node) {
+        $this->addResult($node, \Resultat::MANUEL, 'Vérifier');
+      }
+    }
   }
 }
