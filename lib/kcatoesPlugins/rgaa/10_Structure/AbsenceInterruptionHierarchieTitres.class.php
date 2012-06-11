d09d3b897e8b0c61ce0aed596b69afdf69f8300a
Geoffroy GUERITOT <geoffroy.gueritot@keyconsulting.fr>
2012-06-11 11:56:18 +0200

    maj syntaxique du test rgaa 10.3

--- lib/kcatoesPlugins/rgaa/10_Structure/AbsenceInterruptionHierarchieTitres.class.php
+++ lib/kcatoesPlugins/rgaa/10_Structure/AbsenceInterruptionHierarchieTitres.class.php
@@ -9,10 +9,10 @@ class AbsenceInterruptionHierarchieTitres extends \ASource
 	const testName = 'Absence d\'interruption dans la hiérarchie des titres';
 	const testId = '10.3';
 	protected static $testProc = array(
-     'Si l\'un des éléments mentionnés dans le champ d\'application est présent dans la page, 
+     'Si l\'un des éléments mentionnés dans le champ d\'application est présent dans la page,
       poursuivre le test, sinon le test est non applicable.'
-    ,'Si le premier titre de hiérarchie précédant l\'élément considéré (hn) dans l\'ordre du 
-      code source est de niveau hn, hn-1 ou hn+x (x inférieur ou égal à 4), le test est validé 
+    ,'Si le premier titre de hiérarchie précédant l\'élément considéré (hn) dans l\'ordre du
+      code source est de niveau hn, hn-1 ou hn+x (x inférieur ou égal à 4), le test est validé
       sinon le test est invalidé');
 	protected static $testDocLinks = array(
 		'H42' => 'http://www.w3.org/TR/WCAG20-TECHS/H42.html'
@@ -21,17 +21,18 @@ class AbsenceInterruptionHierarchieTitres extends \ASource
   protected static $testGroups = array(
      'niveau'     => 'A'
     ,'thematique' => 'Structure'
-    ,'profils'    => array('Rédacteur', 'Contributeur', 'Développeur', 'Intégrateur')
+    ,'profils'    => array('Rédacteur', 'Contributeur', 'Développeur',
+    'Intégrateur')
   );
-  
+
 	public function execute()
 	{
 		$crawler = $this->page->crawler;
-		
+
 		$elements = 'h1, h2, h3, h4, h5, h6';
-		
+
 		$nodes = $crawler->filter($elements);
-		
+
 		$nbPb = 0;
 		$cpt = 0;
 		$prevLevel = null;
@@ -48,18 +49,20 @@ class AbsenceInterruptionHierarchieTitres extends \ASource
 			$delta = $level - $prevLevel;
 			if ($delta < -4 || $delta > 1)
 			{
-				$this->addResult($node, \Resultat::ECHEC, 'Rupture constatédans la hiérchie des titres, courant: '.$level.', prédent: '.$prevLevel);
+				$this->addResult($node, \Resultat::ECHEC, 'Rupture constaté dans la
+				hiérarchie des titres, courant: h'.$level.', précédent: h'.$prevLevel);
 				$nbPb++;
 			}
 			$prevLevel = $level;
 			$prevNode = $node;
-			
+
 			$cpt++;
 		}
-		
+
 		if ($nbPb == 0)
 		{
-			$this->addResult(null, \Resultat::REUSSITE, 'Aucune interruption dans la hiérchie des titres');
+			$this->addResult(null, \Resultat::REUSSITE, 'Aucune interruption dans la
+			hiérarchie des titres');
 		}
 	}
 }
