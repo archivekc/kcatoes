afe4361833d9eeb0b5c68de0c98fbf940eaede1c
Geoffroy GUERITOT <geoffroy.gueritot@keyconsulting.fr>
2012-06-26 18:44:17 +0200

    Nettoyage de la classe de test rgaa 3.8

--- lib/kcatoesPlugins/rgaa/03_Formulaires/PresenceAttributLabelElementOptgroup.class.php
+++ lib/kcatoesPlugins/rgaa/03_Formulaires/PresenceAttributLabelElementOptgroup.class.php
@@ -1,18 +1,16 @@
 <?php
 namespace Kcatoes\rgaa;
 
-
-
 class PresenceAttributLabelElementOptgroup extends \ASource
 {
-	
+
   const testName = 'Présence d\'un attribut label sur l\'élément optgroup';
   const testId = '3.8';
   protected static $testProc = array(
-    'Si l\'élément mentionné dans le champ d\'application est présent dans la page, poursuivre le test, sinon le test est non applicable.', 
+    'Si l\'élément mentionné dans le champ d\'application est présent dans la page, poursuivre le test, sinon le test est non applicable.',
     'Si l\'élément possède un attribut label, le test est validé, sinon le test est invalidé.');
   protected static $testDocLinks = array(
-    'H85' => 'http://www.w3.org/TR/WCAG20-TECHS/H85.html'  
+    'H85' => 'http://www.w3.org/TR/WCAG20-TECHS/H85.html'
   );
 
   protected static $testGroups = array(
@@ -20,7 +18,7 @@ class PresenceAttributLabelElementOptgroup extends \ASource
     ,'thematique' => 'Formulaires'
     ,'profils'    => array('Développeur', 'Intégrateur')
   );
-  
+
   public function execute()
   {
     $crawler = $this->page->crawler;
@@ -28,21 +26,22 @@ class PresenceAttributLabelElementOptgroup extends \ASource
     $elements  = 'optgroup';
 
     $nodes = $crawler->filter($elements);
-    
+
     if (count($nodes) == 0) {
     	$this->addResult(null, \Resultat::NA, 'Aucun élément optgroup');
     }
-    
+
     foreach ($nodes as $node)
     {
       $label = $node->getAttribute('label');
       if (strlen(trim($label)) == 0) {
-        $this->addResult($node, \Resultat::ECHEC, 'L\'élément n\'a pas d\'attribut label ou alors celui-ci est vide');
+        $this->addResult($node, \Resultat::ECHEC, 'L\'élément n\'a pas d\'attribut
+        label ou alors celui-ci est vide');
       }
       else {
-        $this->addResult($node, \Resultat::REUSSITE, 'L\'élément a un attribut label non vide : '.$label);
+        $this->addResult($node, \Resultat::REUSSITE, 'L\'élément a un attribut
+        label non vide : '.$label);
       }
     }
-    
   }
 }
