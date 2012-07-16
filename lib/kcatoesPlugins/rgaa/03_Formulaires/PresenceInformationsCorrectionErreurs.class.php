344716a000e1840d65f72247a506bcc9f26b8b6e
Geoffroy GUERITOT <geoffroy.gueritot@keyconsulting.fr>
2012-07-16 10:31:17 +0200

    implementation du test rgaa 3.13

--- lib/kcatoesPlugins/rgaa/03_Formulaires/PresenceInformationsCorrectionErreurs.class.php
+++ lib/kcatoesPlugins/rgaa/03_Formulaires/PresenceInformationsCorrectionErreurs.class.php
@@ -1,8 +1,6 @@
 <?php
 namespace Kcatoes\rgaa;
 
-// FIXME : test à implémenter
-
 class PresenceInformationsCorrectionErreurs extends \ASource
 {
 
@@ -28,26 +26,21 @@ class PresenceInformationsCorrectionErreurs extends \ASource
 
   public function execute()
   {
+    $crawler = $this->page->crawler;
 
-    /*
-      Champ d'application
+    $elements  = 'form';
 
-      Tout élément form.
-     */
+    $nodes = $crawler->filter($elements);
 
-    /*
-      $crawler = $this->page->crawler;
-      $elements = '';
-      $nodes = $crawler->filter($elements);
-
-      $this->addResult($node, \Resultat::ECHEC, '');
-      $this->addResult($node, \Resultat::REUSSITE, '');
-      $this->addResult(null,  \Resultat::NA, '');
-      $this->addResult($node, \Resultat::MANUEL, '');
-
-     */
-
-     $this->addResult(null, \Resultat::MANUEL, 'Pas implémenté');
-
+    if (count($nodes) == 0) {
+       $this->addResult(null, \Resultat::NA, 'Il n\'y a pas de formulaire');
+    }
+    else {
+	    foreach ($nodes as $node)
+	    {
+	      $this->addResult($node, \Resultat::MANUEL, 'Le formulaire propose-t-il des
+	      corrections de saisie ou indique-t-il les formats attendus ?');
+	    }
+    }
   }
 }
